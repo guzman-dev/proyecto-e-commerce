303fe88193d8d1ac46b5541682a5c65674c600b5
vilabruno8 <vilabruno8@gmail.com>
2025-08-14 20:21:39 -0300

    Login.js & Login.html

---
+++ js/login.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('formulario-login');
+  const botonIngresar = document.getElementById("boton-ingresar");
+  const contrasenia = document.getElementById("contrasenia");
+  const usuario = document.getElementById("usuario");
+
+  function validarUsuario() {
+    return usuario.value.trim().length > 0;
+  }
+
+  function validarContrasenia() {
+    return contrasenia.value.length > 6;
+  }
+
+  function validarCredenciales() {
+    if (!validarUsuario() || !validarContrasenia()) {
+        event.preventDefault(); //Previene el redireccionamiento
+        alert("datos inválidos.");
+    }
+  }
+  form.addEventListener('submit', validarCredenciales)
+});
