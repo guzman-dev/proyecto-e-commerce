ae338420ff1b2cadc0127ffafad089711b34e016
Guzmán <gprietoroman3@gmail.com>
2025-09-16 12:00:43 -0300

    Guzman, Abril, Lucas, Bruno, puntos 2 y 3 de la entrega iniciados

--- js/product-info.js
+++ js/product-info.js
@@ -1,13 +1,20 @@
 const idDeProducto = localStorage.getItem('productoSeleccionado');
 const apiURL = "https://japceibal.github.io/emercado-api/products/" + idDeProducto + ".json";
+const apiURLComentarios = "https://japceibal.github.io/emercado-api/products_comments/" + idDeProducto + ".json";
 const carouselInner = document.querySelector('.carousel-inner');
 const productContainer = document.getElementById('producto');
 const productInfo = document.getElementById('productInfo');
+
+const botonEnviarCalificacion = document.getElementById("botonEnviarCalificacion")
+
+const calificaciones = document.getElementById("calificaciones");
 
 const imagen1 = document.getElementById("imagen1");
 const imagen2 = document.getElementById("imagen2");
 const imagen3 = document.getElementById("imagen3");
 const imagen4 = document.getElementById("imagen4");
+
+botonEnviarCalificacion.addEventListener("click", enviarCalificacion);
 
 fetch(apiURL)
    .then(response => {
@@ -35,3 +42,100 @@ fetch(apiURL)
        console.error(error);
        document.body.innerHTML = '<p>Error al cargar el producto.</p>';
    });
+
+let comentarios = [];
+
+fetch (apiURLComentarios)
+    .then(response => {
+        if(!response.ok) throw new Error ("Error al obtener los comentarios");
+        return response.json();
+    })
+    .then(data => {
+        comentarios = data;
+        cargarComentarios();
+    })
+    .catch(error => console.error("Error:", error));
+
+function cargarComentarios(){
+    calificaciones.innerHTML = "";
+    
+    comentarios.forEach(comentario => {
+        const divComentario = document.createElement("div");
+        divComentario.classList.add("d-flex", "flex-column", "card", "mb-3");
+        divComentario.classList.add("card");
+        divComentario.style.backgroundColor="rgb(255, 176, 119, 0.9)";
+
+        const divTopComentario = document.createElement("div");
+        divTopComentario.classList.add("d-flex","flex-row", "gap-4", "align-items-center");
+        divTopComentario.classList.add("card-header");
+
+        const divBottomComentario = document.createElement("div");
+        divBottomComentario.id="divBottomComentario";
+        divBottomComentario.style.backgroundColor="rgb(255, 133, 58, 0.6)";
+        divBottomComentario.classList.add("card-body");
+
+        const nombreUsuario = document.createElement("h5");
+        nombreUsuario.textContent = comentario.user;
+
+        const fechaComentario = document.createElement("p");
+        fechaComentario.textContent = comentario.dateTime;
+
+        const calificacionProducto = document.createElement("p");
+
+        const comentarioProducto = document.createElement("p");
+        comentarioProducto.textContent = comentario.description;
+
+        for (let i = 0; i < 5; i++) {
+            if(i < comentario.score){
+                calificacionProducto.innerHTML += "<span class='fa fa-star checked'></span>"  
+            }else{
+                calificacionProducto.innerHTML += "<span class='fa fa-star'></span>"
+            }
+        }
+
+        divTopComentario.appendChild(nombreUsuario);
+        divTopComentario.appendChild(fechaComentario);
+        divTopComentario.appendChild(calificacionProducto);
+
+        divBottomComentario.appendChild(comentarioProducto);
+
+        divComentario.appendChild(divTopComentario);
+        divComentario.appendChild(divBottomComentario);
+
+        calificaciones.appendChild(divComentario);
+    });
+}   
+
+function enviarCalificacion(){
+        let calificacion = 0;
+        const comentario = document.getElementById("areaParaComentar");
+
+        if(!comentario.value){
+            comentario.placeholder = "Es necesario ingresar un comentario para calificar...";
+            return;
+        }
+
+        document.querySelectorAll('.star-rating:not(.readonly) label').forEach(star => {
+        calificacion += star;
+        });
+
+        if(star === 0){
+            return;
+        }
+
+        cargarComentarios();
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
