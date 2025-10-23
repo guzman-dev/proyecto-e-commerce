69cb900d07b57e0b9e2dacdc2f9595475b0d0677
Guzmán <gprietoroman3@gmail.com>
2025-10-23 10:12:53 -0300

    Modificación de script para botones de navbar

---
+++ js/Nav-Buttons-Manager.js
@@ -0,0 +1,102 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    checkLogin();
+    cargarBotonCart();
+    cargarUsuario();
+    cargarBotonTheme();
+});
+
+
+//COMPRUEBA SI EL USUARIO ESTA LOGEADO, SI NO LO ESTÁ, LO REDERECCIONA A LA PANTALLA DE LOGIN
+
+function checkLogin() {
+    if (localStorage.getItem("loggedIn") == null || localStorage.getItem("loggedIn") === "false") {
+        window.location.href = "login.html";
+    }
+}
+
+//PARA CARGAR EL BOTON DE USUARIO
+
+function cargarUsuario() {
+    if (localStorage.getItem("username") == null) {
+        return;
+    }
+    let navList = document.getElementById("navList");
+
+
+    let elementoDeLista = document.createElement("li");
+    elementoDeLista.classList.add("nav-item");
+
+
+    let nombreDelUsuarioElemento = document.createElement("a");
+    nombreDelUsuarioElemento.classList.add("nav-link");
+
+    nombreDelUsuarioElemento.innerHTML = localStorage.getItem("username");
+    nombreDelUsuarioElemento.href = "my-profile.html";
+
+    elementoDeLista.appendChild(nombreDelUsuarioElemento);
+
+    navList.appendChild(elementoDeLista);
+
+}
+
+//PARA CARGAR EL BOTON DE MODO OSCURO/CLARO
+function cargarBotonTheme(){
+    const html = document.documentElement;
+    let theme = localStorage.getItem("theme") || "claro";
+    html.setAttribute("data-theme", theme);
+    const themeActual = html.getAttribute('data-theme');
+
+
+    let navList = document.getElementById("navList");
+
+
+    let elementoDeLista = document.createElement("li");
+    elementoDeLista.classList.add("nav-item");
+
+
+    let boton = document.createElement("button");
+    boton.addEventListener("click", cambiarTheme);
+    boton.classList.add("nav-link");
+    boton.setAttribute("id", "botonTheme");
+
+    boton.innerHTML = themeActual === "oscuro" ? "Modo: Oscuro" : "Modo: Claro";
+
+    elementoDeLista.appendChild(boton);
+
+    navList.appendChild(elementoDeLista);
+}
+
+//FUNCIONALIDAD DEL BOTON OSCURO/CLARO
+function cambiarTheme(event){
+    boton = event.currentTarget;
+    html = document.documentElement;
+    const themeActual = html.getAttribute('data-theme');
+    const themeNuevo = themeActual === "claro" ? "oscuro" : "claro";
+
+    localStorage.setItem("theme", themeNuevo);
+    boton.innerHTML = localStorage.getItem("theme") === "oscuro" ? "Modo: Oscuro" : "Modo: Claro";
+
+
+    html.setAttribute("data-theme", themeNuevo);
+}
+
+//PARA CARGAR EL BOTON DEL CARRITO
+function cargarBotonCart(){
+    let navList = document.getElementById("navList");
+
+
+    let elementoDeLista = document.createElement("li");
+    elementoDeLista.classList.add("nav-item");
+
+
+    let botonDeCarrito = document.createElement("a");
+    botonDeCarrito.classList.add("nav-link");
+
+    botonDeCarrito.innerHTML = "Carrito";
+    botonDeCarrito.href = "cart.html";
+
+    elementoDeLista.appendChild(botonDeCarrito);
+
+    navList.appendChild(elementoDeLista);
+}
