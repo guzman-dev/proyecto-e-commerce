691dbbd065816a46ed40ebc4bfabd9c8f37bbf3e
Guzmán <gprietoroman3@gmail.com>
2025-10-28 17:47:30 -0300

    Borré comentario

--- js/cart.js
+++ js/cart.js
@@ -1,33 +1,4 @@
-/*
-<div class="card mb-3 divDeProducto snes-container my-3" style="max-width: 100%;">
-        <div class="row g-0">
-          <div class="col-md-4 bg-dark d-flex justify-content-center align-items-center divImagenProducto">
-            <img src="${producto.imagen}" class="imgProducto img-fluid" alt="${producto.nombre}">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <div class="d-flex nombreYCantidad justify-content-between flex-column flex-lg-row">
-                <h5>${producto.nombre}</h5>
-                <div>
-                  <label>Cantidad:</label>
-                  <input type="number" name="cantidadField" id="cantidadField" value="${producto.cantidad}" style="width: 10%;">
-                </div>
-              </div>
-              <div class="precioYMoneda my-2">
-                <h5>${producto.moneda} ${producto.precio}</h5>
-              </div>
-              <div class="bottomDiv d-flex justify-content-between mt-2 mt-md-5 flex-column flex-md-row">
-                <div class="subtotal">
-                  <h5>Subtotal: ${producto.precio * producto.cantidad}</h5>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-*/
 
-//Eso es el código html para cada casilla, los datos que tenes que completar están entre 4 #'s,
 //Lo único que falta es que el Total no desaparezca cuando la pantilla es mediana o pequeña, y el mensaje cuando no hay productos en el carrito.
 
 const contenedorProductos = document.getElementById("listadoCarrito");
