3683de4b7dfb2ae19b1122a2a040a56b911c52be
Guzmán <gprietoroman3@gmail.com>
2025-10-30 09:06:20 -0300

    Elementos del carrito alineados

--- js/cart.js
+++ js/cart.js
@@ -32,31 +32,31 @@ function mostrarProductosEnCarrito() {
     divProducto.style.maxWidth = "100%";
 
 
-    divProducto.innerHTML = ` <div class="row g-0">
+    divProducto.innerHTML = ` <div class="row g-0 container">
           <div class="col-md-4 d-flex justify-content-center align-items-center divImagenProducto">
             <img src="${producto.imagen}" class="imgProducto img-fluid" alt="${producto.nombre}">
           </div>
-          <div class="col-md-8">
+          <div class="col-md-8 contenedorInfoProducto">
             <div class="card-body">
               <div class="d-flex nombreYCantidad justify-content-between flex-column flex-lg-row">
                 <h5>${producto.nombre}</h5>
                 <div style="padding: 0">
                   <label>Cantidad:</label>
-                  <input type="number" name="cantidadField" class="cantidadField" value="${producto.cantidad}" style="width: 10%;">
+                  <input type="number" name="cantidadField" class="cantidadField" value="${producto.cantidad}" style="width: 5vw;">
                 </div>
               </div>
               <div class="precioYMoneda my-2">
                 <h5>${producto.moneda} ${producto.precio}</h5>
               </div>
-              <div class="bottomDiv d-flex justify-content-between mt-2 mt-md-5 flex-column flex-md-row">
+              <div class="bottomDiv d-flex justify-content-between mt-2 mt-md-5 flex-column flex-md-row ">
                 <div class="subtotal">
                   <h5 class="subTotal" id="subTotal">Subtotal: ${producto.precio * producto.cantidad}</h5>
                 </div>
-                <div class="snes-button" id="quitarProd">
+                <div class="snes-button " id="quitarProd">
                 <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                 <path d="M16 2v4h6v2h-2v14H4V8H2V6h6V2h8zm-2 2h-4v2h4V4zm0 4H6v12h12V8h-4zm-5 2h2v8H9v-8zm6 0h-2v8h2v-8z" fill="currentColor"/>
                 </svg>
-              </div>
+                </div>
               </div>
             </div>
           </div>
@@ -74,7 +74,7 @@ function mostrarProductosEnCarrito() {
       subTotal.innerHTML = "Subtotal: " + producto.precio * inputCantidad.value;
 
       cargarTotal();
-      
+
       //actualizar badge live
 
       producto.cantidad = parseInt(inputCantidad.value);
@@ -82,17 +82,17 @@ function mostrarProductosEnCarrito() {
 
       actualizarBadgeCarrito();
     });
-    
+
     const quitarProducto = divProducto.querySelector("#quitarProd");
 
-      quitarProducto.addEventListener("click", () => {
+    quitarProducto.addEventListener("click", () => {
       divProducto.remove();
       const index = productos.indexOf(producto);
       if (index > -1) productos.splice(index, 1);
       localStorage.setItem("productosEnCarrito", JSON.stringify(productos));
       actualizarBadgeCarrito();
       cargarTotal();
-      });
+    });
 
   });
 
@@ -108,3 +108,7 @@ function mostrarProductosEnCarrito() {
     total.innerHTML = "Total: " + totalNumero;
   }
 }
+
+btnSeguirComprando.addEventListener("click", () => {
+  window.location = "products.html";
+})
